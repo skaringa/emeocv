064ec060f38995629be583c133b883f3b7c5e538
nl04303 <44981174+nl04303@users.noreply.github.com>
2019-01-12 20:37:20 +0100

    Update Config.cpp

--- Config.cpp
+++ Config.cpp
@@ -10,7 +10,9 @@
 Config::Config() :
         _rotationDegrees(0), _ocrMaxDist(5e5), _digitMinHeight(20), _digitMaxHeight(
                 90), _digitYAlignment(10), _cannyThreshold1(100), _cannyThreshold2(
-                200), _trainingDataFilename("trainctr.yml") {
+                200), _trainingDataFilename("trainctr.yml") ,_digitNum(6),
+                _divisionNum(1), _coordX(0), _coordY(0), _coordW(480), _coordH(640),
+                _imgW(480), _imgH(640), _oneFile(0), _negativeNum(0), _rollAvgInt(10) {
 }
 
 void Config::saveConfig() {
@@ -23,6 +25,17 @@ void Config::saveConfig() {
     fs << "digitYAlignment" << _digitYAlignment;
     fs << "ocrMaxDist" << _ocrMaxDist;
     fs << "trainingDataFilename" << _trainingDataFilename;
+    fs << "digitNum" << _digitNum;
+    fs << "divisionNum" << _divisionNum;
+    fs << "coordX" << _coordX;
+    fs << "coordY" << _coordY;
+    fs << "coordW" << _coordW;
+    fs << "coordH" << _coordH;
+    fs << "imgW"   << _imgW;
+    fs << "imgH"   << _imgH;
+    fs << "oneFile" << _oneFile;
+    fs << "negativeNum" << _negativeNum;
+    fs << "rollAvgInt" << _rollAvgInt;
     fs.release();
 }
 
@@ -37,6 +50,17 @@ void Config::loadConfig() {
         fs["digitYAlignment"] >> _digitYAlignment;
         fs["ocrMaxDist"] >> _ocrMaxDist;
         fs["trainingDataFilename"] >> _trainingDataFilename;
+        fs["digitNum"] >>_digitNum;
+        fs["divisionNum"] >> _divisionNum;
+        fs["coordX"] >> _coordX;
+        fs["coordY"] >> _coordY;
+        fs["coordW"] >> _coordW;
+        fs["coordH"] >> _coordH;
+        fs["imgW"] >> _imgW;
+        fs["imgH"] >> _imgH;
+        fs["oneFile"] >> _oneFile;
+        fs["negativeNum"] >> _negativeNum;
+        fs["rollAvgInt"] >> _rollAvgInt;
         fs.release();
     } else {
         // no config file - create an initial one with default values
